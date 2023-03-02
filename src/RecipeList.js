8e5e86a381d3c28a1733e222f08ff2d710af70c6
kolozsvarikrisztian <laszlokrisztiankolozsvari@gmail.com>
2023-03-02 07:55:10 +0100

    0301

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,13 +1,15 @@
-import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faPencilAlt, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Fragment, useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Spinner } from "./Spinner";
+import { Modal } from "./Modal";
 
 
 export function RecipeList() {
     const [recipes, setRecipes] = useState([]);
     const [isPending, setPending] = useState(false);
+    const [deleteId, setDeleteId] = useState("");
 
     function fetchRecipes() {
         return fetch(process.env.REACT_APP_BACKEND_URL + "/api/recipes")
@@ -28,6 +30,16 @@ export function RecipeList() {
 
     return (
         <Fragment>
+            {deleteId !== "" ? (
+                <Modal
+                    onApproved = {async () => {
+                        setPending(true);
+                        await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/recipes/${deleteId}`, {method: "DELETE"});
+                        await fetchRecipes();
+                        setPending(false);
+                    }}
+                    onClosed = {() => setDeleteId("") }>Biztosan törlöd a {recipes.find((recipe) => recipe.id === deleteId) ?.name} recepjét?</Modal>
+            ): ( "" )}
             {alignToRows(recipes).map((row, i) => (
                 <div className="row mb-3" key={i}>
                     {row.map((recipe) => (
@@ -43,6 +55,16 @@ export function RecipeList() {
                                             <FontAwesomeIcon icon={faPencilAlt} />
                                         </button>
                                     </NavLink>
+                                    <NavLink to={`/recept/${recipe.slug}`}>
+                                        <button className="btn btn-sm btn-outline-primary">
+                                            <FontAwesomeIcon icon={faEye} />
+                                        </button>
+                                    </NavLink>
+                                    {/* <NavLink to={`/recept/${recipe.slug}`}> */}
+                                        <button className="btn btn-sm btn-outline-danger ml-2" onClick={() => {setDeleteId(recipe.id)}}>
+                                            <FontAwesomeIcon icon={faTimes} />
+                                        </button>
+                                    {/* </NavLink> */}
                                 </div>
                             </div>
                         </div>
